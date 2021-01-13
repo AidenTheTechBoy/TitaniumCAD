d010d7f4c732cf7b3b9cdd02082c9d849ed89d05
Aiden <iheartchaya@gmail.com>
2021-01-12 21:27:30 -0500

    API status monitoring.

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,41 @@
+//Config for .env Files
 require('dotenv').config()
 
+//Imports
+//const eventLoopStats = require('event-loop-stats')
 const rateLimit = require('express-rate-limit')
 const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 
+//Active Status Monitor
+app.use(require('express-status-monitor')())
+
+//Serve Static Directory
 app.use('/static', express.static(__dirname.replace(/\\/g, '/') + `/routes/public`))
 
+//Setup JSON Body Parser
 app.use(bodyParser.json({
     verify: (req, res, buf) => {
         req.rawBody = buf
     }
 }))
 
+//Setup Cors
 app.use(cors())
 
 // 1000 req / 5 min
-app.use("/", rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 1000
-}))
+// app.use("/", rateLimit({
+//     windowMs: 5 * 60 * 1000,
+//     max: 1000
+// }))
 
 // 10000 req / 1 Day
-app.use("/", rateLimit({
-    windowMs: 24 * 60 * 60 * 1000,
-    max: 10000
-}))
+// app.use("/", rateLimit({
+//     windowMs: 24 * 60 * 60 * 1000,
+//     max: 10000
+// }))
 
 let num = 1
 app.use("/", async function (req, res, next) {
@@ -69,13 +78,6 @@ app.use('/api', apiRoutes)
 
 const { uploadRoutes } = require('./routes/upload')
 app.use('/upload', uploadRoutes)
-
-// var pidusage = require('pidusage')
-// setInterval(() => {
-//     pidusage(process.pid, function (err, stats) {
-//         console.log(stats)
-//     })
-// }, 1000)
 
 app.listen(5000, () => {
     console.log('API online. Awaiting Requests...')
